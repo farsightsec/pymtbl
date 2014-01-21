9f8835ca5c9698294df20612f8a0a44fcc2412c9
Robert Edmonds <edmonds@fsi.io>
2014-01-21 18:18:37 -0500

    setup.py: add NAME, VERSION, and only skip cython rebuild if mtbl.c is available

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 
+NAME = 'pymtbl'
+VERSION = '0.2'
+
 from distutils.core import setup
 from distutils.extension import Extension
+import os
 
 try:
     from Cython.Distutils import build_ext
     setup(
-        name = 'mtbl',
+        name = NAME,
+        version = VERSION,
         ext_modules = [ Extension('mtbl', ['mtbl.pyx'], libraries = ['mtbl']) ],
         cmdclass = {'build_ext': build_ext},
     )
 except ImportError:
-    setup(
-        name = 'mtbl',
-        ext_modules = [ Extension('mtbl', ['mtbl.c'], libraries = ['mtbl']) ],
-    )
+    if os.path.isfile('mtbl.c'):
+        setup(
+            name = NAME,
+            version = VERSION,
+            ext_modules = [ Extension('mtbl', ['mtbl.c'], libraries = ['mtbl']) ],
+        )
+    else:
+        raise
