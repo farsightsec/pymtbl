8f71cd2f77f5b6974660f0b291a555b9cd1d581e
Robert Edmonds <edmonds@fsi.io>
2014-04-04 18:00:26 -0400

    setup.py: use pkg-config to retrieve -I, -L, -l flags for libmtbl

--- setup.py
+++ setup.py
@@ -5,22 +5,37 @@ VERSION = '0.2'
 
 from distutils.core import setup
 from distutils.extension import Extension
-import os
+
+def pkgconfig(*packages, **kw):
+    import subprocess
+    flag_map = {
+            '-I': 'include_dirs',
+            '-L': 'library_dirs',
+            '-l': 'libraries'
+    }
+    pkg_config_cmd = 'pkg-config --cflags --libs "%s"' % ' '.join(packages)
+    for token in subprocess.check_output(pkg_config_cmd, shell=True).split():
+        flag = token[:2]
+        arg = token[2:]
+        if flag in flag_map:
+            kw.setdefault(flag_map[flag], []).append(arg)
+    return kw
 
 try:
     from Cython.Distutils import build_ext
     setup(
         name = NAME,
         version = VERSION,
-        ext_modules = [ Extension('mtbl', ['mtbl.pyx'], libraries = ['mtbl']) ],
+        ext_modules = [ Extension('mtbl', ['mtbl.pyx'], **pkgconfig('libmtbl')) ],
         cmdclass = {'build_ext': build_ext},
     )
 except ImportError:
+    import os
     if os.path.isfile('mtbl.c'):
         setup(
             name = NAME,
             version = VERSION,
-            ext_modules = [ Extension('mtbl', ['mtbl.c'], libraries = ['mtbl']) ],
+            ext_modules = [ Extension('mtbl', ['mtbl.c'], **pkgconfig('libmtbl')) ],
         )
     else:
         raise
