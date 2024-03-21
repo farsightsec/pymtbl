d3fc0fe3ad947694a644b4a5e00b00c56c5f063e
Maximillian Crawford <mcrawford@domaintools.com>
2024-03-21 17:21:34 -0400

    Updated setup.py version

--- setup.py
+++ setup.py
@@ -13,36 +13,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-NAME = 'pymtbl'
-VERSION = '0.5.1'
-LICENSE = 'Apache License 2.0'
-DESCRIPTION = 'Python extension module for the mtbl C library'
-URL = 'https://github.com/farsightsec/pymtbl'
-AUTHOR = 'Farsight Security, Inc.'
-AUTHOR_EMAIL = 'software@farsightsecurity.com'
+NAME = "pymtbl"
+VERSION = "0.6.0"
+LICENSE = "Apache License 2.0"
+DESCRIPTION = "Python extension module for the mtbl C library"
+URL = "https://github.com/farsightsec/pymtbl"
+AUTHOR = "Farsight Security, Inc."
+AUTHOR_EMAIL = "software@farsightsecurity.com"
 
 
 from distutils.core import setup, Command
 from distutils.extension import Extension
 import unittest
 
+
 def pkgconfig(*packages, **kw):
     import subprocess
-    flag_map = {
-            '-I': 'include_dirs',
-            '-L': 'library_dirs',
-            '-l': 'libraries'
-    }
+
+    flag_map = {"-I": "include_dirs", "-L": "library_dirs", "-l": "libraries"}
 
     pkg_config_cmd = (
-        'pkg-config',
-        '--cflags',
-        '--libs',
-        ' '.join(packages),
+        "pkg-config",
+        "--cflags",
+        "--libs",
+        " ".join(packages),
     )
 
-    pkg_config_output = subprocess.check_output(pkg_config_cmd,
-                                                universal_newlines=True)
+    pkg_config_output = subprocess.check_output(pkg_config_cmd, universal_newlines=True)
 
     for token in pkg_config_output.split():
         flag = token[:2]
@@ -54,6 +51,7 @@ def pkgconfig(*packages, **kw):
 
 class Test(Command):
     user_options = []
+
     def initialize_options(self):
         pass
 
@@ -62,32 +60,37 @@ class Test(Command):
 
     def run(self):
         unittest.TextTestRunner(verbosity=1).run(
-            unittest.TestLoader().discover('tests'))
+            unittest.TestLoader().discover("tests")
+        )
 
 
 try:
     from Cython.Distutils import build_ext
+
     setup(
-        name = NAME,
-        version = VERSION,
+        name=NAME,
+        version=VERSION,
         license=LICENSE,
         description=DESCRIPTION,
         url=URL,
         author=AUTHOR,
         author_email=AUTHOR_EMAIL,
-        ext_modules = [ Extension('mtbl', ['mtbl.pyx'], **pkgconfig('libmtbl >= 1.1.0')) ],
-        cmdclass = {
-            'build_ext': build_ext,
-            'test': Test,
+        ext_modules=[Extension("mtbl", ["mtbl.pyx"], **pkgconfig("libmtbl >= 1.1.0"))],
+        cmdclass={
+            "build_ext": build_ext,
+            "test": Test,
         },
     )
 except ImportError:
     import os
-    if os.path.isfile('mtbl.c'):
+
+    if os.path.isfile("mtbl.c"):
         setup(
-            name = NAME,
-            version = VERSION,
-            ext_modules = [ Extension('mtbl', ['mtbl.c'], **pkgconfig('libmtbl >= 1.1.0')) ],
+            name=NAME,
+            version=VERSION,
+            ext_modules=[
+                Extension("mtbl", ["mtbl.c"], **pkgconfig("libmtbl >= 1.1.0"))
+            ],
         )
     else:
         raise
